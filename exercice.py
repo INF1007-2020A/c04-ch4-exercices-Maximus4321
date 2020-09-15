3d428e99714d42bbdcaa5f4d478b344edc074ab5
Maximus4321 <maxime.appert@polymtl.ca>
2020-09-15 23:05:25 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -3,10 +3,10 @@
 
 
 def is_even_len(string: str) -> bool:
-    if len(string)%2=0:
+    if len(string)%2=0 :
     return True
     else :
-    false
+    retrun false
 
 
 def remove_third_char(string: str) -> str:
