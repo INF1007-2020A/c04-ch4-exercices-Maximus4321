2d1036a74f691f0dbd9ee28a9f5165ad03352403
Maximus4321 <maxime.appert@polymtl.ca>
2020-09-15 23:28:58 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -24,8 +24,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 def get_number_of_char(string: str, char: str) -> int:
     count = 0
-  
-for i in test_str: 
+  for i in test_str: 
     if i == 'e': 
         count = count + 1
 
