560db2053ad3958c0e42abc084b4b7ea0e11990d
Maximus4321 <maxime.appert@polymtl.ca>
2020-09-15 22:48:21 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -7,7 +7,7 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    return print (str [;2,]) 
+    return print (str [;2,4]) 
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
