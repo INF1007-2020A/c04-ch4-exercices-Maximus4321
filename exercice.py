9736417c3666a47597559c776091215bbd4cbaa9
Maximus4321 <maxime.appert@polymtl.ca>
2020-09-15 23:36:46 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -32,7 +32,7 @@ def get_number_of_char(string: str, char: str) -> int:
 
 def get_number_of_words(sentence: str, word: str) -> int:
     number = -1
-    for i in sentence: 
+    for i in chaine: 
         if i == word: 
          number = number + 1
     return number
