cad052f59351dd052bd98120c8f5804e3cba9779
Maximus4321 <maxime.appert@polymtl.ca>
2020-09-15 23:24:14 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -16,7 +16,7 @@ def remove_third_char(string: str) -> str:
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    myString.index
+    string.index()
 
 
 def get_number_of_char(string: str, char: str) -> int:
